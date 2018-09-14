ce4e8f326ff8ecaee7068d01cc9d76f7ac3c7c2f
paczek137 <paczek137@gmail.com>
2018-09-15 00:51:22 +0200

    added auto logging and posting in fb group

--- p3_facebook.py
+++ p3_facebook.py
@@ -1,6 +1,9 @@
-import datetime
+import datetime, time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 s = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
 s = s + "\np3_facebook.py"
@@ -23,17 +26,51 @@ driver.close()
 
 '''
 
-login =  "+48782143060"
+login =  "+48516022766"
 password = "qwerty0987"
 
-driver = webdriver.Firefox()
+fp = webdriver.FirefoxProfile()
+fp.set_preference("permissions.default.desktop-notification", 0)
+fp.set_preference("dom.webnotifications.enabled", False)
+#cap = webdriver.DesiredCapabilities.FIREFOX.copy()
+
+
+
+driver = webdriver.Firefox(firefox_profile=fp)
 driver.get("https://www.facebook.com/")
 print("Opened facebook")
+driver.maximize_window()
+
 
 a = driver.find_element_by_id('email')
+print(a)
+#aa = driver.find_element(By.CSS_SELECTOR, '#m_login_email')
+aa = driver.find_element_by_xpath('//*[@id="email"]')
+print(aa)
 a.send_keys(login)
 b = driver.find_element_by_id('pass')
 b.send_keys(password)
 c = driver.find_element_by_id('loginbutton')
 c.click()
 
+
+#driver.get("https://www.facebook.com/groups/1888323521464203/")
+driver.get("https://www.facebook.com/groups/288421491988781/")
+driver.execute_script("window.scrollTo(0, 500);")
+
+#for writting on the wall
+#wall = driver.find_element_by_xpath("//textarea[@name='xhpc_message']")
+#time.sleep(5)
+#p = driver.find_element_by_xpath("//textarea[@name='message']")
+p = driver.find_element_by_name('xhpc_message_text')
+
+p.send_keys('Hi 6')
+
+time.sleep(3)
+element = WebDriverWait(driver, 30).until(expected_conditions.element_to_be_clickable((By.XPATH, '//button/span[.=\"Post\"]')))
+#element = WebDriverWait(driver, 30).until(expected_conditions.element_to_be_clickable((By.XPATH, "//span[text()='Post']")))
+
+element.click()
+
+#postbutton = driver.find_element_by_xpath("//button[contains(.,'Post')]")
+#postbutton.click()
